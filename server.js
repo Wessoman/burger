e0948bb94c2d17ba2364ca79a4277c2387e5a78c
Antony Hernandez <wessoman@hotmail.com>
2019-07-11 23:24:24 -0500

    Ver .04

--- server.js
+++ server.js
@@ -4,8 +4,33 @@
 
 var express = require("express");
 var bodyParser = require("body-parser");
-var methodOverride = require("method-override");
+
+// Set Handlebars.
 var exphbs = require("express-handlebars");
 
 var PORT = process.env.PORT || 8080;
 var app = express();
+
+// Serve static content for the app from the "public" directory in the application directory.
+//adding __dirname + to public is what allowed me access to the images in assets/img folder
+app.use(express.static(__dirname + "/public"));
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// parse application/json
+app.use(bodyParser.json());
+
+
+//sets main.handlebars as the default layout and our view engine as handlebar
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+//import routes and give the server access to them
+var routes = require("./controllers/burgersController.js");
+
+app.use(routes);
+
+app.listen(PORT, function() {
+  console.log("App now listening at localhost:" + PORT);
+});
