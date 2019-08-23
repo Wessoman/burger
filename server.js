5ea3e8642cf2b2eeac8cae82cda07b478bb97866
Antony Hernandez <wessoman@hotmail.com>
2019-08-23 10:30:32 -0500

    server port set to 3000

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var bodyParser = require("body-parser");
 var methodOverride = require("method-override");
 var exphbs = require("express-handlebars");
 
-var PORT = process.env.PORT || 8080;
+var PORT = process.env.PORT || 3000;
 var app = express();
 
 // Serve static content for the app from the "public" directory in the application directory.
